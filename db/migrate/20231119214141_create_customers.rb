44a0a5ed8f52f3b5477d9b9344effaa3f614d1a1
John Santosuosso <john.santosuosso1@gmail.com>
2023-11-19 16:45:09 -0500

    Add validations to Customer Model

--- db/migrate/20231119214141_create_customers.rb
+++ db/migrate/20231119214141_create_customers.rb
@@ -1,14 +1,16 @@
 class CreateCustomers < ActiveRecord::Migration[5.2]
   def change
     create_table :customers do |t|
-      t.string :first_name
-      t.string :string
-      t.string :last_name
-      t.string :string
-      t.string :email
-      t.string :username
+      t.string :first_name , null: false
+      t.string :string , null: false
+      t.string :last_name , null: false
+      t.string :string , null: false
+      t.string :email , null: false
+      t.string :username , null: false
 
-      t.timestamps
+      t.timestamps null: false
     end
+    add_index :customers, :email, unique: true
+    add_index :customers, :username, unique: true
   end
 end
